268a376fa2dea5bbade84902b0d4447f08fed9e0
Ivy <donglinda345@gmail>
2026-04-29 00:03:50 +0800

    optimize the path of test data

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -15,16 +15,15 @@ class TestLogin:
         self.swag_labs_proxy = SwagLabsProxy(self.driver)
 
     @pytest.mark.parametrize("username, password, keywords",
-                             get_json_data(r"D:\my_files\projects\saucedemo_playwright\data\test_login.json"))
+                             get_json_data("../data/test_login.json"))
     def test_login_success(self, username, password, keywords):
         """Test successful login - runs with fresh browser"""
         self.login_proxy.login(username, password)
         assert is_element_present(self.driver, keywords)
         self.swag_labs_proxy.logout()
-        print("-------11111-------")
 
     @pytest.mark.parametrize("username, password, keywords",
-                             get_json_data(r"D:\my_files\projects\saucedemo_playwright\data\test_login_failed.json"))
+                             get_json_data("../data/test_login_failed.json"))
     def test_login_failed(self, username, password, keywords):
         """Test failed login - runs with fresh browser"""
         self.login_proxy.login(username, password)
